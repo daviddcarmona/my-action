2dc08bf572cf50df58d3725334418beb82acc37a
daviddcarmona <david.carmona@du.edu>
2023-02-09 09:44:01 -0700

    fix: octokit

--- src/action.js
+++ src/action.js
@@ -7,15 +7,12 @@ async function run(){
 
     const { context = {}} = github;
     const { pull_request } = context.payload;
-
-    await octokit.issues.createComment({
+    
+    await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
         body: 'Thank you for submitting a pull request!'
-
     })
-
-    console.log('Hello World!')
 }
 
 run()
