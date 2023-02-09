ae10157953af7eab41590c7447d429d10058e7f5
daviddcarmona <david.carmona@du.edu>
2023-02-09 10:24:35 -0700

    fix: octokit

--- src/action.js
+++ src/action.js
@@ -7,11 +7,16 @@ const octokit = github.getOctokit(GITHUB_TOKEN);
 const { pull_request } = context.payload;
  
 async function run() {
-  await octokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
-  });
+    try {
+        await octokit.rest.issues.createComment({
+          ...context.repo,
+          issue_number: pull_request.number,
+          body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+        });
+        
+    } catch (error) {
+        console.log(error)
+    }
 }
  
 run();
